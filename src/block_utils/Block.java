ac809d9e28df1a856da5c92010c42ce049d7d166
axlmk <axel-gauthier@hotmail.fr>
2019-02-25 14:12:51 +0100

    Ecriture fonctions block_repair et write_block

--- src/block_utils/Block.java
+++ src/block_utils/Block.java
@@ -4,19 +4,69 @@ import java.io.*;
 
 public class Block {
 	
-	private byte [] data = new byte[4];
 	private final static int BLOCK_SIZE = 4;
-	
-	public Block(byte [] data) {
-		this.data = data;
+	private byte [] data = new byte[BLOCK_SIZE];
+
+	public Block() {
+		
 	}
+	
+	/**
+	 * @author axel
+	 * @param pos Position où écrire le block
+	 * @param disk_id Disk sur lequel écrire le block	
+	 * @return Un entier indiquant si l'opération s'est bien passée (0 : OK, 1 : Erreur cast, 2 ; Erreur écriture) 
+	 */
 	
 	public int write_block(int pos, File disk_id)  {
-		return 0;
+		FileOutputStream fos = null;
+		try {
+			fos = new FileOutputStream(disk_id);
+		} catch (FileNotFoundException e1) {
+			System.err.println("Le fichier n'a pas ete correctement ouvert en ecriture.");
+			e1.printStackTrace();
+			return 2;
+		}
+		try {
+			fos.write(this.data, pos, BLOCK_SIZE);
+			fos.close();
+			return 0;
+		} catch (IOException e) {
+			System.err.println("Une erreur est survenue lors de l'ecriture du fichier.");
+			e.printStackTrace();
+			return 1;
+		}
 	}
 	
-	public int block_repair(int pos, File [] disks, int disk_id) {
-		return 0;
+	/**
+	 * @author axel
+	 * @param pos Position du block eronné
+	 * @param disks Tableau des disks utilisés par le RAID
+	 * @param id_disk Index du disk avec le block eronné
+	 * @param nbr_disks
+	 * @return Un entier indiquant si l'opération s'est bien passée (0 : OK, 1 : Erreur lecture, 2 : Erreur écriture)
+	 */
+	
+	public int block_repair(int pos, File [] disks, int id_disk, int nbr_disks) {
+		Block block = new Block();
+		
+		for(int i = 0;i < nbr_disks; i++) {
+			if(i != id_disk) {
+				if(!block.read_block(pos, disks[i])) {
+					System.err.println("Une erreur est survenue, il y a au moins deux blocks qui ont ete corrompus.");
+					return 1;
+				}
+				
+				for(int j = 0;j < BLOCK_SIZE; j++) {
+					this.data[i] = (byte)(this.data[i] ^ block.data[i]);
+				}
+			}
+		}
+		
+		if(this.write_block(pos, disks[id_disk]) == 0) {
+			return 0;
+		}
+		return 2;
 	}
 
 }
