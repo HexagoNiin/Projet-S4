e4a84de9ab9ea04c8983b9b3e9adf379935ae62f
axlmk <axel-gauthier@hotmail.fr>
2019-02-25 14:18:26 +0100

    Correctif block_repair

--- src/utils_block.c
+++ src/utils_block.c
@@ -17,19 +17,22 @@ int write_block(block_t block, int pos, int disk_id) {
 int block_repair(int pos, int disks[], int id_disk, int nbr_disks) {
     /// \brief Répare un block erroné
     /// \param[in] pos : Position du block eronné
-    /// \param[in] disks : Tableau des disks utilisés par le RAID
+    /// \param[in, out] disks : Tableau des disks utilisés par le RAID
     /// \param[in] disk_id : Index du disk avec le block eronné
+    /// \param[in] nbr_disks : Nombre de disks utilisés par le RAID
     /// \return Un entier indiquant si l'opération s'est bien passée
     int i, j;
     block_t block_repare;
     block_t * block;
     for(i=0;i<nbr_disks;i++) {//nbr_disks à transformer en DEFINE ?
-        if(!read_block(block, pos, disks[i])) {
-            fprintf(stderr, "Une erreur est survenue, il y a au moins deux blocks qui ont ete corrompus.\n");
-            return 1;
+        if(i != id_disk) {
+            if(!read_block(block, pos, disks[i])) {
+                fprintf(stderr, "Une erreur est survenue, il y a au moins deux blocks qui ont ete corrompus.\n");
+                return 1;
+            }
+            for(j=0;j<BLOCK_SIZE;j++)
+                block_repare.data[j] ^= block->data[j];
         }
-        for(j=0;j<BLOCK_SIZE;j++)
-            block_repare.data[j] ^= block->data[j];
     }
     if(!write_block(block_repare, pos, disks[id_disk])) {
         return 0;
