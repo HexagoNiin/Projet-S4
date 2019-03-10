06904318f3d7345418599ba7129c9aa6d7b69cd7
HexagoNiin <niin@free.fr>
2019-03-10 21:45:30 +0100

    Documentation header utils_inode.h

--- headers/utils_inode.h
+++ headers/utils_inode.h
@@ -1,3 +1,9 @@
+/// \file utils_inode.h
+/// \brief Utilitaires permettant de manipuler la structure inode_s
+/// \author Ugo EB-LEVADOUX
+/// \author Axel GAUTHIER
+/// \author Cédric MARTIN
+/// \date 2019
 #ifndef _UTILS_INODE
 #define _UTILS_INODE
 
