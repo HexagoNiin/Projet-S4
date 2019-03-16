cfb37a45e919b5e07608bee77ede388e5961fc91
axlmk <axel-gauthier@hotmail.fr>
2019-03-16 14:57:28 +0000

    Ajout des commentaires doxygen sur les fonctions get_unused_inode et write_inodes_table (et ses dependances)

--- src/utils_inode.c
+++ src/utils_inode.c
@@ -1,6 +1,9 @@
 #include "../headers/utils_inode.h"
 
 uchar *indtostr(inode_t inode) {
+    /// \brief Transforme une inode en chaine de caractères.
+    /// \param[in] inode : L'inode a transformer
+    /// \return La chaine de caractères ou NULL s'il y a eu une erreur
     if(sizeof(inode_t) > MAX_MSG) {
         fprintf(stderr, "L'inode ne peut pas etre convertie en string.\n");
         return NULL;
@@ -32,6 +35,10 @@ uchar *indtostr(inode_t inode) {
 }
 
 int write_inodes_table(inode_table_t inodes, FILE **files) {
+    /// \brief Ecrit la table d'inode sur le système RAID.
+    /// \param[in] inodes : La table d'inodes
+    /// \param[in, out] files : Le système RAID
+    /// \return 0 si tout s'est bien passé, 1 s'il y a eu une erreur lors du cast de la table, 2 s'il y a eu erreur lors de l'écriture
     int i, nStripe = 0;
     for(i=0;i<INODE_TABLE_SIZE;i++) {
         uchar *buffer = indtostr(inodes[i]);
@@ -48,6 +55,9 @@ int write_inodes_table(inode_table_t inodes, FILE **files) {
 }
 
 int get_unused_inode(inode_table_t inodes) {
+    /// \brief Retourne l'indice du premier inode disponible dans la table.
+    /// \param[in] inodes : Table d'inodes
+    /// \return L'indice d'un inode ou -1 si la table est pleine
     int i;
     for(i=0;i<INODE_TABLE_SIZE;i++) {
         if(inodes[i].first_byte == 0) {
