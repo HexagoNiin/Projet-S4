5b4e805b36529d1db6e7dcde8c7ac41f926cda3c
axlmk <axel-gauthier@hotmail.fr>
2019-04-27 14:37:12 +0200

    Ajout du fichier utils_os et mise en place de l'interpreteur

---
+++ headers/utils_os.h
@@ -0,0 +1,26 @@
+#ifndef _UTILS_OS
+#define _UTILS_OS
+
+#include <string.h>
+#include "utils_file.h"
+
+#define NB_COMMANDS 7
+#define COMMANDS_SIZE 16
+#define NB_OPTIONS 1
+
+typedef int(*FunctionStr)(char*);
+
+int action(char **command);
+int cat(char *filename);
+int ls(char *option);
+int rm(char *filename);
+int create(char *filename);
+int edit(char *filename);
+int load(char *filename);
+int store(char *filename);
+
+
+void interpreteur();
+char **parser(char *command);
+
+#endif
