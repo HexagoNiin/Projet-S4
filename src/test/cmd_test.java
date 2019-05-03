6489dfaaecd5f88a67da9f73143d0b3cbf534778
HexagoNiin <niin@free.fr>
2019-05-03 18:46:34 +0200

    Vraie fonction de test pour les blocs

--- src/test/cmd_test.java
+++ src/test/cmd_test.java
@@ -1,12 +1,18 @@
 package test;
 
-import objets.Chunk;
+import objets.*;
 
 public class cmd_test {
 
 	public static void main(String[] args) {
-		Chunk chaine = new Chunk("banane", 6);
-		chaine.write_chunk(0);
+		VirtualDisk r5Disk = new VirtualDisk("systeme");
+		Block b = new Block();
+		b.setByte((byte) 1, 0);
+		b.setByte((byte) 2, 1);
+		b.setByte((byte) 3, 2);
+		b.setByte((byte) 4, 3);
+		b.write_block(0, r5Disk.getStorage()[0]);
+		b.read_block(0, r5Disk.getStorage()[0]);
 	}
 
 }
