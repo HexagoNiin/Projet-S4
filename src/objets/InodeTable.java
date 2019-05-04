b49068acb566d1f08bbc86f3e808f2e1455231d8
HexagoNiin <niin@free.fr>
2019-05-04 17:43:35 +0200

    InodeTable.delete(...)

--- src/objets/InodeTable.java
+++ src/objets/InodeTable.java
@@ -15,6 +15,25 @@ public class InodeTable {
 		return 0;
 	}
 	
+	public int delete(int pos) {
+		for(int i = pos; i <= nbInodes; i++) {
+			tab[i] = tab[i++];
+		}
+		nbInodes--;
+		return 0;
+	}
+	
+	public int delete(Inode inode) {
+		int i = 0;
+		while(tab[i] != inode && i < nbInodes) { i++; }
+		if(i == nbInodes) { return -1; }
+		for(int j = i; j <= nbInodes; j++) {
+			tab[j] = tab[j++];
+		}
+		nbInodes--;
+		return 0;
+	}
+	
 	public int getUnused() {
 		for(int i = 0; i < tabSize; i++) {
 			if(tab[i].getFirstByte() == 0) {
