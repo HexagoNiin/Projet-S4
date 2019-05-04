d25c9820eb6bffd6933803fa96529dfc4e8b7d82
HexagoNiin <niin@free.fr>
2019-05-04 20:13:49 +0200

    Remaniement cmd_test

--- src/test/cmd_test.java
+++ src/test/cmd_test.java
@@ -4,21 +4,17 @@ import objets.*;
 
 public class cmd_test {
 
-	public static void main(String[] args) {
-		VirtualDisk r5Disk = new VirtualDisk("systeme");
+	public static void main(String[] args) {	
+		new VirtualDisk("systeme");
 		
 		System.out.println("Écriture d'un block :");
-		Block b = new Block();
-		b.setByte((byte) 1, 0);
-		b.setByte((byte) 2, 1);
-		b.setByte((byte) 3, 2);
-		b.setByte((byte) 4, 3);
-		b.display();
+		Block b = new Block(1);
+		System.out.println(b.toString());
 		b.write(0, VirtualDisk.storage[0]);
 		
 		System.out.println("Lecture d'un block :");
 		b.read(0, VirtualDisk.storage[0]);
-		b.display();
+		System.out.println(b.toString());
 		
 		System.out.println();
 	}
