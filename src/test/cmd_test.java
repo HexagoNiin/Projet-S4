ccd8353bf3a7be6ec43d2914c70ddbb9011d94b6
HexagoNiin <niin@free.fr>
2019-05-05 03:17:41 +0200

    Update cmd_test.java

--- src/test/cmd_test.java
+++ src/test/cmd_test.java
@@ -7,16 +7,30 @@ public class cmd_test {
 	public static void main(String[] args) {	
 		new VirtualDisk("systeme");
 		
+		/* BLOCK */
 		System.out.println("…criture d'un block :");
-		Block b = new Block(1);
-		System.out.println(b.toString());
-		b.write(0, VirtualDisk.storage[0]);
+		Block b1 = new Block(1);
+		System.out.println(b1);
+		b1.write(0, VirtualDisk.storage[0]);
 		
-		System.out.println("Lecture d'un block :");
-		b.read(0, VirtualDisk.storage[0]);
-		System.out.println(b.toString());
+		System.out.println("\nLecture d'un block :");
+		Block b2 = new Block();
+		b2.read(0, VirtualDisk.storage[0]);
+		System.out.println(b2);
 		
-		System.out.println();
+		/* STRIPE */
+		System.out.println("\n…criture d'une stripe :");
+		byte chaine[] = "123456789abc".getBytes();
+		Stripe s1 = new Stripe(chaine);
+		System.out.println(s1);
+		s1.write(0);
+		
+		System.out.println("\nLecture d'un block :");
+		Stripe s2 = new Stripe();
+		s2.read(0);
+		System.out.println(s2);
+		
+		/* CHUNK */
 	}
 
 }
