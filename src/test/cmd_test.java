342baab80bbbdbcc40a9428018f729d7f6bfb6cf
HexagoNiin <niin@free.fr>
2019-05-05 03:48:36 +0200

    Update cmd_test.java

--- src/test/cmd_test.java
+++ src/test/cmd_test.java
@@ -23,14 +23,30 @@ public class cmd_test {
 		byte chaine[] = "123456789abc".getBytes();
 		Stripe s1 = new Stripe(chaine);
 		System.out.println(s1);
+		Stripe s3 = new Stripe(chaine);
+		System.out.println(s3);
+		Stripe s4 = new Stripe(chaine);
+		System.out.println(s4);
+		Stripe s5 = new Stripe(chaine);
+		System.out.println(s5);
 		s1.write(0);
 		
-		System.out.println("\nLecture d'un block :");
+		System.out.println("\nLecture d'une stripe :");
 		Stripe s2 = new Stripe();
 		s2.read(0);
 		System.out.println(s2);
 		
 		/* CHUNK */
+		System.out.println("\n…criture d'un chunk :");
+		String message = "J'aime les bananes !";
+		Chunk c1 = new Chunk(message);
+		System.out.println(c1.content() + " :\n" +c1);
+		c1.write(0);
+		
+		System.out.println("\nLecture d'un chunk :");
+		Chunk c2 = new Chunk(message.length());
+		c2.read(0);
+		System.out.println(c2);
 	}
 
 }
