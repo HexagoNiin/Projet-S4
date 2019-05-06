1704166c0332d9a6538c200eabb5d7a9a960a906
gogonouze <47778065+gogonouze@users.noreply.github.com>
2019-05-06 19:32:53 +0200

    Update Utils.java

--- src/objets/Utils.java
+++ src/objets/Utils.java
@@ -1,6 +1,17 @@
 package objets;
 
+
+/**
+ * @author MARTIN Cedric, GAUTHIER Axel && EB-LEVADOUX Ugo
+ *	
+ */
 public class Utils {
+	
+	/**
+	 * give number of block for nb_octes bytes
+	 * @param nb_octets number of byte
+	 * @return number of block
+	 */
 	public static int compute_nblock(int nb_octets) {
 		int nb_blocks = nb_octets / 4;
 		if(nb_octets % 4 != 0) {
@@ -10,26 +21,30 @@ public class Utils {
 	}
 	
 	/**
-	 * Calcule le nombre de blocks necessaires pour ecrire une chaine de bytes sur le systeme RAID
-	 * Prend en compte les blocks de parites.
-	 * @param nb_disks Nombre de disks du systeme RAID.
-	 * @return Nombre de blocks.
+	 * give number of stripe for nb_octes bytes
+	 * @param nb_octets number of byte
+	 * @return number of stripe
 	 */
-	/*public int compute_final_nblock(int nb_disks) {
-		int nChunks = this.compute_nblock();
-		int nStripes = this.compute_nstripe(nChunks);
-		return nChunks + nStripes + ((nChunks + nStripes) / nb_disks);
-	}*/
-
-	
 	public static int compute_nstripe(int nb_blocks) {
 		return nb_blocks / (VirtualDisk.nDisk - 1) + ((nb_blocks % (VirtualDisk.nDisk - 1) != 0) == true ? 1 : 0);
 	}
 	
+	/**
+	 * position of parity block for stripe
+	 * @param numBande stripe's position
+	 * @return parity block's position 
+	 */
 	public static int compute_parity_index(int numBande) {
 		return (VirtualDisk.nDisk - 1) - (numBande % VirtualDisk.nDisk);
 	}
 	
+	/**
+	 * resize original array from begin position with size size 
+	 * @param original resizing array
+	 * @param begin resizing position
+	 * @param size resizing size
+	 * @return new SubArray
+	 */
 	public static byte [] subArray(byte [] original, int begin, int size) {
 		byte newArray[] = new byte [size];
 		for(int i = 0; i < size; i++) {
