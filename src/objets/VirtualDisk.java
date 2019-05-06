edbd5d65c5f7cfd83d4f2cc625aa8e0e9e241fa6
gogonouze <47778065+gogonouze@users.noreply.github.com>
2019-05-06 19:52:36 +0200

    Update VirtualDisk.java

--- src/objets/VirtualDisk.java
+++ src/objets/VirtualDisk.java
@@ -2,6 +2,11 @@ package objets;
 
 import java.io.*;
 
+
+/**
+ * @author MARTIN Cedric, GAUTHIER Axel && EB-LEVADOUX Ugo
+ *	
+ */
 public class VirtualDisk {
 	public static int numberOfFiles;
 	public static SuperBlock superBlock;
@@ -11,6 +16,10 @@ public class VirtualDisk {
 	public static File [] storage = new File[nDisk]; //tab[NUMBER_OF_DISKS];
 	public static int nextParityPos = nDisk-1;
     
+    /**
+     * VirtualDisk creation at nom_rep directory
+     * @param nom_rep name of directory
+     */
     public VirtualDisk(String nom_rep) {
 		File repertoire = new File(nom_rep);
 		if(!(repertoire.exists() && repertoire.isDirectory())) {
@@ -20,25 +29,11 @@ public class VirtualDisk {
 		nextParityPos = nDisk-1;
     	storage = repertoire.listFiles();
     	nDisk = storage.length;
-    	/*Path chemin = Paths.get(nom_rep);
-    	try (DirectoryStream<Path> stream = Files.newDirectoryStream(chemin)) {
-    		int tailleStorage = 0;
-    		for (Path file: stream) {
-    	        tailleStorage++;
-    	    }
-    		storage = new File [tailleStorage];
-    		tailleStorage = 0;
-    	    for (Path file: stream) {
-    	        storage[tailleStorage] = file;
-    	        tailleStorage++;
-    	    }
-    	} catch (IOException | DirectoryIteratorException x) {
-    	    // IOException can never be thrown by the iteration.
-    	    // In this snippet, it can only be thrown by newDirectoryStream.
-    	    System.err.println(x);
-    	}*/ //JE SAIS PAS LIRE DES DOSSIERS EN JAVA C'EST L'ENFER
     	numberOfFiles = 0;
     }
     
+    /**
+     * VirtualDisk creation
+     */
     public VirtualDisk() {}
 }
